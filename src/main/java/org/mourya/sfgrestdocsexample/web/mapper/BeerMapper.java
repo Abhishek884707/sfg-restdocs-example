9defe8792a96adf21f71c4830d089058dce8da86
Abhishek Kumar <2021mt70042@wilp.bits-pilani.ac.in>
2023-12-06 18:58:24 +0530

    Implemented MapStruct

---
+++ src/main/java/org/mourya/sfgrestdocsexample/web/mapper/BeerMapper.java
@@ -0,0 +1,16 @@
+package org.mourya.sfgrestdocsexample.web.mapper;
+
+import org.mapstruct.Mapper;
+import org.mourya.sfgrestdocsexample.domain.Beer;
+import org.mourya.sfgrestdocsexample.web.model.BeerDto;
+
+/**
+ * Created by jt on 2019-05-25.
+ */
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+
+    BeerDto BeerToBeerDto(Beer beer);
+
+    Beer BeerDtoToBeer(BeerDto dto);
+}
---
+++ target/generated-sources/annotations/org/mourya/sfgrestdocsexample/web/mapper/BeerMapperImpl.java
@@ -0,0 +1,68 @@
+package org.mourya.sfgrestdocsexample.web.mapper;
+
+import javax.annotation.processing.Generated;
+import org.mourya.sfgrestdocsexample.domain.Beer;
+import org.mourya.sfgrestdocsexample.web.model.BeerDto;
+import org.mourya.sfgrestdocsexample.web.model.BeerStyleEnum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-12-06T18:57:36+0530",
+    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 17.0.6 (Oracle Corporation)"
+)
+@Component
+public class BeerMapperImpl implements BeerMapper {
+
+    @Autowired
+    private DateMapper dateMapper;
+
+    @Override
+    public BeerDto BeerToBeerDto(Beer beer) {
+        if ( beer == null ) {
+            return null;
+        }
+
+        BeerDto.BeerDtoBuilder beerDto = BeerDto.builder();
+
+        beerDto.id( beer.getId() );
+        if ( beer.getVersion() != null ) {
+            beerDto.version( beer.getVersion().intValue() );
+        }
+        beerDto.createdDate( dateMapper.asOffsetDateTime( beer.getCreatedDate() ) );
+        beerDto.lastModifiedDate( dateMapper.asOffsetDateTime( beer.getLastModifiedDate() ) );
+        beerDto.beerName( beer.getBeerName() );
+        if ( beer.getBeerStyle() != null ) {
+            beerDto.beerStyle( Enum.valueOf( BeerStyleEnum.class, beer.getBeerStyle() ) );
+        }
+        beerDto.upc( beer.getUpc() );
+        beerDto.price( beer.getPrice() );
+
+        return beerDto.build();
+    }
+
+    @Override
+    public Beer BeerDtoToBeer(BeerDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Beer.BeerBuilder beer = Beer.builder();
+
+        beer.id( dto.getId() );
+        if ( dto.getVersion() != null ) {
+            beer.version( dto.getVersion().longValue() );
+        }
+        beer.createdDate( dateMapper.asTimestamp( dto.getCreatedDate() ) );
+        beer.lastModifiedDate( dateMapper.asTimestamp( dto.getLastModifiedDate() ) );
+        beer.beerName( dto.getBeerName() );
+        if ( dto.getBeerStyle() != null ) {
+            beer.beerStyle( dto.getBeerStyle().name() );
+        }
+        beer.upc( dto.getUpc() );
+        beer.price( dto.getPrice() );
+
+        return beer.build();
+    }
+}
